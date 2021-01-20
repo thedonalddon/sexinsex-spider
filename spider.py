a120e1b57ea592ce095ac15806423580efbdf771
Donald <43942741+thedonalddon@users.noreply.github.com>
2021-01-20 14:52:01 +0800

    Add files via upload

--- spider.py
+++ spider.py
@@ -9,7 +9,9 @@ import re
 import urllib.request
 import urllib.error
 import xlwt
-# import sqlite3
+import datetime
+import xlrd
+import xlsxwriter
 
 # 定义全局变量和搜索方法
 threadlist = []
@@ -22,16 +24,18 @@ findJpeg = re.compile(r'src="(.*?).jpeg"')
 findTitle = re.compile(r'</a>(.*?)</h1>')
 findTorrent = re.compile(r'<a href="attachment.php\?aid=(.*?)" target=')
 
-baseurl = input("输入要爬取第地址（如http://sexinsex.net/bbs/forum-143-）：") # 填入爬取地址
+baseurl = input("输入要爬取的地址（如亚洲原创http://sexinsex.net/bbs/forum-143-）（或亚洲转帖http://sexinsex.net/bbs/forum-25-）：")  # 填入爬取地址
 # pages = 15
-pages = input('从第2页开始爬，要爬到几页？')  #填入页数
+pages = input('从第2页开始爬，要爬到几页？')  # 填入页数
 pages = int(pages)
 
-
+xlsname = ''
+now = str(datetime.datetime.now())
+xls = now[:-7] + '.xls'
 def main():
     threadlist = getThreadList(baseurl)
     content = getContent()
-    savepath = "result.xls"
+    savepath = xls
     saveData(content, savepath)
 
 
@@ -59,8 +63,10 @@ def getContent():
     content = []
     i = 0
     while i <= len(threadlist)-1:
-        print('开始爬取第%d个内容页' % i)
+        jindu = i *100 // len(threadlist)
+        print('开始爬取第', i, '个内容页，', '共', len(threadlist), '个内容页，进度：', jindu, '%')
         html = askURL(threadlist[i])
+        content.append(threadlist[i])
         # 逐一解析数据
         soup = BeautifulSoup(html, "html.parser")
         try:
@@ -72,33 +78,29 @@ def getContent():
                 torrent = re.findall(findTorrent, item)[0]      # 获取种子
                 content.append('http://sexinsex.net/bbs/attachment.php?aid='+torrent)
         except Exception as result:
-            print('Exception occurs:')
+            print('title/torrent Exception occurs:')
             print(result)
         try:
-            for item1 in soup:
-                item1 = str(item1)
-                # print(item1)
-                jpg = re.findall(findJpg, item1)                  # 获取jpg图片
-                # print(jpg)
-                if jpg:                                           # 除空
-                    content.append(jpg[0]+'.jpg')
-                else:
-                    continue
+            jpg = re.findall(findJpg, item)                         # 获取jpg图片
+            # print(jpg)
+            if jpg:                                                 # 除空
+                q = 0
+                while q < len(jpg):
+                    content.append(jpg[q] + '.jpg')
+                    q += 1
         except Exception as result:
-            print('Exception occurs:')
+            print('jpg Exception occurs:')
             print(result)
         try:
-            for item1 in soup:
-                item1 = str(item1)
-                # print(item1)
-                jpeg = re.findall(findJpeg, item1)                # 获取jpg图片
-                # print(jpeg)
-                if jpeg:                                          # 除空
-                    content.append(jpeg[0]+'.jpeg')
-                else:
-                    continue
+            jpeg = re.findall(findJpeg, item)                         # 获取jpeg图片
+            # print(jpg)
+            if jpeg:                                                 # 除空
+                q = 0
+                while q < len(jpeg):
+                    content.append(jpeg[q] + '.jpg')
+                    q += 1
         except Exception as result:
-            print('Exception occurs:')
+            print('jpeg Exception occurs:')
             print(result)
         i += 1
     # print(content)
@@ -132,8 +134,33 @@ def saveData(content, savepath):
     for i in range(0, len(content)):
         print('第%d条数据写入中' % i)
         sheet.write(i, 0, content[i])
-    book.save('result.xls')
+    book.save(savepath)
+
+    booka = xlrd.open_workbook(savepath, 'r')  # 打开.xlsx文件
+    sheeta = booka.sheets()[0]  # 打开表格中第一个sheet
+    editedxls = 'edited-'+savepath
+    bookb = xlsxwriter.Workbook(editedxls)  # 打开新xlsx文件
+    sheetb = bookb.add_worksheet('帖子列表')
+
+    nrows = sheeta.nrows  # 获取总行数
+    rowAlist = []
+
+    for i in range(0, nrows):
+        rowAcontent = sheeta.cell(i, 0).value  # 遍历A列所有数据
+        sheetb.write(i, 0, rowAcontent)
+        if 'jpg' in rowAcontent:
+            sheetb.write(i, 1, rowAcontent)
+    bookb.close()
+
+    print('数据已保存为 %s 。Congrats! 下一步：执行convertimg.py ' % editedxls)
     # 批量下载图片
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
